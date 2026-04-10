b5c475beb3a7f151890422b0424ffdf56f20d0c7
Rifqi Arfani <aryarfani@gmail.com>
2026-04-10 14:31:26 +0700

    Add color support for Labels in Label.js
    - Introduced a predefined array of colors for Labels.
    - Updated the hexColor property to use the color from the array based on the provided colorIndex in labelData.
    - Enhanced the Label class to improve visual representation of labels.

--- src/structures/Label.js
+++ src/structures/Label.js
@@ -4,6 +4,29 @@ const Base = require('./Base');
 // eslint-disable-next-line no-unused-vars
 const Chat = require('./Chat');
 
+const Colors = [
+    "#ff9485",
+    "#64c4ff",
+    "#ffd429",
+    "#dfaef0",
+    "#99b6c1",
+    "#55ccb3",
+    "#ff9dff",
+    "#d3a91d",
+    "#6d7cce",
+    "#d7e752",
+    "#00d0e2",
+    "#ffc5c7",
+    "#93ceac",
+    "#f74848",
+    "#00a0f2",
+    "#83e422",
+    "#ffaf04",
+    "#b5ebff",
+    "#9ba6ff",
+    "#9368cf"
+]
+
 /**
  * WhatsApp Business Label information
  */
@@ -35,7 +58,7 @@ class Label extends Base {
          * Label hex color
          * @type {string}
          */
-        this.hexColor = labelData.hexColor;
+        this.hexColor = Colors[labelData.colorIndex];
     }
     /**
      * Get all chats that have been assigned this Label
@@ -44,6 +67,7 @@ class Label extends Base {
     async getChats() {
         return this.client.getChatsByLabelId(this.id);
     }
+
 }
 
 module.exports = Label;
